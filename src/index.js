9ee2a1ae36b86a41524bfa244ff2a9ccc9aa5967
findus19 <ivanexs@mail.ru>
2020-03-13 14:45:52 +0300

    day2 diplom

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@
 import openPhone from './modules/openPhone';
 import dialogMenu from './modules/dialogMenu';
 import scroll from './modules/scroll';
+import maskPhone from './modules/maskPhone';
 
 openPhone();
 
 dialogMenu();
 
 scroll();
+
+maskPhone();
